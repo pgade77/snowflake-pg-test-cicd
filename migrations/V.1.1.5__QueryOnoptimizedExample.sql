a657c060bd050319290d3f705be36fe68bcc9ccf
pgade77 <gphani@live.com>
2021-12-16 12:32:50 -0500

    commit

---
+++ migrations/V.1.1.5__QueryOnoptimizedExample.sql
@@ -0,0 +1,5 @@
+use role sysadmin;
+use warehouse DBT_DEV_HEAVY_WH;
+use database pg_second_db;
+use schema   second_db;
+create or replace temporary table numbers_test as    select seq4() as number    from table(generator(rowcount => 1000000000));
