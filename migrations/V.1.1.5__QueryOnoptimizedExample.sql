6a6a7039bd3a957f281a43f0a436922bb8d9f10a
pgade77 <gphani@live.com>
2021-12-16 12:35:41 -0500

    temporary to permanent

--- migrations/V.1.1.5__QueryOnoptimizedExample.sql
+++ migrations/V.1.1.5__QueryOnoptimizedExample.sql
@@ -2,4 +2,4 @@ use role sysadmin;
 use warehouse DBT_DEV_HEAVY_WH;
 use database pg_second_db;
 use schema   second_db;
-create or replace temporary table numbers_test as    select seq4() as number    from table(generator(rowcount => 1000000000));
+create or replace table numbers_test as    select seq4() as number    from table(generator(rowcount => 1000000000));
